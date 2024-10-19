ccb1f73023e9609ee5d2250fdf4ab8174b64b975
kulisi <492378124@163.com>
2024-10-19 23:48:26 +0800

    优化代码SheinAESTools

--- aestools.go
+++ aestools.go
@@ -7,16 +7,38 @@ import (
 	"errors"
 )
 
-type AESTools struct {
+type SheinAESTools struct {
 	blockLength int
 	ivLength    int
 }
 
-func NewDefaultAESTools() *AESTools {
-	return &AESTools{128, 16}
+func NewDefaultAESTools() *SheinAESTools {
+	return &SheinAESTools{128, 16}
+}
+func (t SheinAESTools) Decrypt(content string, key string) ([]byte, error) {
+	// content Base64(IV+AES密文)
+	if content != "" && len(content) != 0 && key != "" && len(key) != 0 {
+		// 对content 进行 base64 解密
+		decode, err := base64.StdEncoding.DecodeString(content)
+		if err != nil {
+			return nil, err
+		}
+		// 验证 密文长度
+		if len(decode) <= t.ivLength {
+			return nil, errors.New("错误的密文")
+		} else {
+			var ivBytes = make([]byte, t.ivLength)
+			var realDate = make([]byte, len(decode)-t.ivLength)
+			ivBytes = decode[:t.ivLength]
+			realDate = decode[t.ivLength:]
+			return t.decrypt(realDate, []byte(key)[:t.ivLength], ivBytes)
+		}
+	} else {
+		return nil, errors.New("密文和密钥不能为空")
+	}
 }
 
-func (t AESTools) Decrypt(content string, key string) (string, error) {
+func (t SheinAESTools) DecryptString(content string, key string) (string, error) {
 	// content Base64(IV+AES密文)
 	if content != "" && len(content) != 0 && key != "" && len(key) != 0 {
 		// 对content 进行 base64 解密
@@ -32,26 +54,27 @@ func (t AESTools) Decrypt(content string, key string) (string, error) {
 			var realDate = make([]byte, len(decode)-t.ivLength)
 			ivBytes = decode[:t.ivLength]
 			realDate = decode[t.ivLength:]
-			return t.decrypt(realDate, []byte(key)[:t.ivLength], ivBytes)
+			b, err := t.decrypt(realDate, []byte(key)[:t.ivLength], ivBytes)
+			return string(b), err
 		}
 	} else {
 		return "", errors.New("密文和密钥不能为空")
 	}
 }
 
-func (t AESTools) decrypt(content, key, iv []byte) (string, error) {
+func (t SheinAESTools) decrypt(content, key, iv []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	mode := cipher.NewCBCDecrypter(block, iv)
 	result := make([]byte, len(content))
 	mode.CryptBlocks(result, content)
 	result = t.pkcs5UnPadding(result)
-	return string(result), nil
+	return result, nil
 }
 
-func (t AESTools) pkcs5UnPadding(origData []byte) []byte {
+func (t SheinAESTools) pkcs5UnPadding(origData []byte) []byte {
 	length := len(origData)
 	unpadding := int(origData[length-1])
 	return origData[:(length - unpadding)]
